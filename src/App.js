d4ba5c5cd8dfb74a97e44bcccf27276fad5cf860
sleem3232 <sleem3232@gmail.com>
2022-04-11 06:11:57 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios, { Axios } from 'axios';
+import { useState } from 'react';
+const API=axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com',
+  timeout:30000,
+});
 function App() {
+const [apirespons,setapirespose]=useState({})
+const [loading,setloading]=useState(false)
+  const makegetapicall=()=>{
+    setloading(true);
+  axios.request({
+    method:'GET',
+    url:'https://jsonplaceholder.typicode.com/todos',
+
+  }).then(res=>{
+    setapirespose(res.data)
+  }).catch(err=>{
+    setapirespose(err)
+  }).finally(()=>{
+    setloading(false)
+  });
+  }
+  const makepostapi=()=>{
+    setloading(true);
+    API.post("/posts",{
+      titel:"my custome titel",
+      completed:false,
+      
+    })
+    .then((res)=>{
+      setapirespose(res.data);
+    })
+    .catch((err)=>{
+      setapirespose(err);
+    })
+    .finally(()=>{
+      setloading(false);
+    });
+    
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <button onClick={makegetapicall}>Get api</button>
+   <button onClick={makepostapi}>make post api</button>
+   
+   <div>{loading?"loading..":  JSON.stringify(apirespons)}</div>
     </div>
   );
 }
